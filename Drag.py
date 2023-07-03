f0f5106dd153409e4025e4e8e511cdbd82502e1d
Nisha Bhakar <69414363+Nishabhakar@users.noreply.github.com>
2023-07-03 14:47:15 +0530

    Update Drag.py

--- Drag.py
+++ Drag.py
@@ -12,8 +12,6 @@ def process_uploaded_file(uploaded_file):
         df = pd.read_excel(uploaded_file, engine='openpyxl')
     else:
         return None
-        
-    df.drop_duplicates(inplace=True) # remove duplicate values
     
     return df
 
@@ -29,19 +27,22 @@ def upload_data():
             
             if df is not None:
                 merged_df = pd.concat([merged_df, df], ignore_index=True)
-                merged_df.drop_duplicates(inplace=True) # remove duplicate values
         
         if not merged_df.empty:
-            merged_df.drop_duplicates(inplace=True)  # Remove duplicates from the merged DataFrame
+            # Remove duplicates from the merged DataFrame
+            merged_df.drop_duplicates(inplace=True)
             
             # Apply filtering
             st.sidebar.header("Please filter here:")
             country = st.sidebar.multiselect("Select the country:", options=merged_df["Country"].unique(), default=[], key="country_dropdown")
             network = st.sidebar.multiselect("Select the network:", options=merged_df["Network"].unique(), default=merged_df["Network"].unique())
             
+            # Filter the data
             filtered_df = merged_df[(merged_df["Country"].isin(country)) & (merged_df["Network"].isin(network))]
-    
             
+            # Remove duplicate rows from the filtered data
+            filtered_df.drop_duplicates(inplace=True)
+
             if not filtered_df.empty:
                 # Calculate minimum price in USD from merged data
                 min_price = filtered_df['Price in USD'].min()
@@ -49,20 +50,17 @@ def upload_data():
                 # Display minimum price
                 st.write("Minimum Price (USD):", min_price)
 
-
                 # Display the minimum price row with specified column styles
                 min_row = filtered_df.loc[filtered_df['Price in USD'] == min_price, ["Country", "Network", "Price in USD", "Service Provider"]]
-
+                min_row = min_row.drop_duplicates()  # Remove duplicate rows
                 min_row_styled = min_row.style.apply(lambda x: ['background-color: black; color: white'] * len(x), axis=0)
                 st.header("Minimum price in USD")
-                min_row_styled.drop_duplicates(inplace=True)
                 st.dataframe(min_row_styled)
                 
                 # Display the filtered data table
                 st.write('<style> th {background-color: black; color: white;} </style>', unsafe_allow_html=True)
                 st.dataframe(filtered_df)
             
-                
                 # Download filtered data as Excel
                 output_file = io.BytesIO()
                 filtered_df.to_excel(output_file, index=False, header=True)
