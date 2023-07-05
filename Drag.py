04045b9569710d28f8ef5f5e3022bf2ab2e4670c
Nisha Bhakar <69414363+Nishabhakar@users.noreply.github.com>
2023-07-05 10:41:47 +0530

    Update Drag.py

--- Drag.py
+++ Drag.py
@@ -32,14 +32,26 @@ def upload_data():
             # Remove duplicates from the merged DataFrame
             merged_df.drop_duplicates(inplace=True)
             
-            # Apply filtering
+           # Apply filtering
             st.sidebar.header("Please filter here:")
-            country = st.sidebar.multiselect("Select the country:", options=merged_df["Country"].unique(), default=[], key="country_dropdown")
-            network = st.sidebar.multiselect("Select the network:", options=merged_df["Network"].unique(), default=merged_df["Network"].unique())
+            country_options = ['Select All'] + merged_df["Country"].unique().tolist()
+            country = st.sidebar.multiselect("Select the country:", options=country_options, default=[], key="country_dropdown")
+            if "Select All" in country:
+                country = merged_df["Country"].unique().tolist()  # Select all countries
             
+            network_options = ['Select All'] + merged_df["Network"].unique().tolist()
+            network = st.sidebar.multiselect("Select the network:", options=network_options, default=[], key="network_dropdown")
+            if "Select All" in network:
+                network = merged_df["Network"].unique().tolist()  # Select all networks
+            # st.sidebar.header("Please filter here:")
+            # country = st.sidebar.multiselect("Select the country:", options=merged_df["Country"].unique(), default=[], key="country_dropdown")
+            # #country = st.sidebar.multiselect("Select the country:", options=merged_df["Country"].unique(), default=merged_df["Country"].unique())
+            # network = st.sidebar.multiselect("Select the network:", options=merged_df["Network"].unique(), default=merged_df["Network"].unique())
+
             # Filter the data
             filtered_df = merged_df[(merged_df["Country"].isin(country)) & (merged_df["Network"].isin(network))]
-            
+            filtered_df = filtered_df.sort_values(by='Price in USD')
+
             # Remove duplicate rows from the filtered data
             filtered_df.drop_duplicates(inplace=True)
 
@@ -50,28 +62,49 @@ def upload_data():
                 # Display minimum price
                 st.write("Minimum Price (USD):", min_price)
 
-                # Display the minimum price row with specified column styles
+
+                # Display the minimum price rows with specified column styles
                 min_row = filtered_df.loc[filtered_df['Price in USD'] == min_price, ["Country", "Network", "Price in USD", "Service Provider"]]
-                min_row = min_row.drop_duplicates()  # Remove duplicate rows
-                min_row_styled = min_row.style.apply(lambda x: ['background-color: black; color: white'] * len(x), axis=0)
-                st.header("Minimum price in USD")
-                st.dataframe(min_row_styled)
-                
-                # Display the filtered data table
-                st.write('<style> th {background-color: black; color: white;} </style>', unsafe_allow_html=True)
-                st.dataframe(filtered_df)
+                min_rows_styled = min_row.style.apply(lambda x: ['background-color: black; color: white'] * len(x), axis=0)
+                st.header("Minimum price in USD by Country")
+                st.dataframe(min_rows_styled)
             
-                # Download filtered data as Excel
-                output_file = io.BytesIO()
-                filtered_df.to_excel(output_file, index=False, header=True)
-                output_file.seek(0)
-                b64 = base64.b64encode(output_file.read()).decode()
-                href = f'<a href="data:application/octet-stream;base64,{b64}" download="filtered_data.xlsx">Download Filtered Data</a>'
-                st.markdown(href, unsafe_allow_html=True)
                 
-                st.success("Filtered data is ready for download.")
-            else:
-                st.warning("No data found after filtering.")
+            # Get the rows with the minimum price for each country
+            min_default = merged_df.loc[merged_df.groupby('Country')['Price in USD'].idxmin()]
+            min_default = min_default.sort_values(by='Price in USD')
+
+            min_default_styled = min_default.style.apply(lambda x: ['background-color: black; color: white'] * len(x), axis=0)
+            st.header("Minimum price in USD in all Countries")
+            st.dataframe(min_default_styled)
+
+            # Download filtered data as Excel
+            output_file = io.BytesIO()
+            min_default.to_excel(output_file, index=False, header=True)
+            output_file.seek(0)
+            b64 = base64.b64encode(output_file.read()).decode()
+            href = f'<a href="data:application/octet-stream;base64,{b64}" download="min_data.xlsx">Download min_Data</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+            st.success("Data is ready for download.")
+
+            # Display the filtered data table
+            st.header("Filtered Data")
+            st.write('<style> th {background-color: black; color: white;} </style>', unsafe_allow_html=True)
+            st.dataframe(filtered_df)
+
+            # Download filtered data as Excel
+            output_file = io.BytesIO()
+            filtered_df.to_excel(output_file, index=False, header=True)
+            output_file.seek(0)
+            b64 = base64.b64encode(output_file.read()).decode()
+            href = f'<a href="data:application/octet-stream;base64,{b64}" download="filtered_data.xlsx">Download Filtered Data</a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+            st.success("Filtered data is ready for download.")
+        else:
+            st.warning("No data found after filtering.")
+        
 
 # Run the application
 if __name__ == '__main__':
